c5087e103321295b8667d99219ee42ccf900b5bb
HynTrann <tranhinpyn175069@gmail.com>
2022-10-25 17:11:52 +0700

    Delete space

--- assets/js/login.js
+++ assets/js/login.js
@@ -65,14 +65,14 @@ document.querySelector("form#login").onsubmit = (event) => {
   let email = form.emailLogin.value.trim();
   let password = form.passwordLogin.value.trim();
   signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    localStorage.setItem("user", email)
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage)
-  });
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      localStorage.setItem("user", email)
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+    });
 }
